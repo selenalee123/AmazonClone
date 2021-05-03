cc986a0a82da333fbce6e30543720cd23cb7733c
Kim Kim <52686208+selenalee123@users.noreply.github.com>
2021-05-03 16:51:23 -0400

    navigation and shopping cart

---
+++ src/screens/ShoppingCartScreen/index.tsx
@@ -0,0 +1,56 @@
+﻿import React from 'react';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import CartProductItem from '../../components/CartProductItem';
+import Button from '../../components/Button';
+
+import products from '../../data/cart';
+
+interface Props {
+
+}
+
+const ShoppingCartScreen = (props: Props) => {
+    const totalPrice = products.reduce((summedPrice, product) => summedPrice + product.item.price * product.quantity, 0,);
+    // const navigation = useNavigation();
+    const onCheckout = () => {
+        console.log('Address');
+      };
+    return (
+        <View style={styles.page}>
+            {/* Render Product Component */}
+            <FlatList
+        data={products}
+        renderItem={({item}) => <CartProductItem cartItem={item} />}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={() => (
+          <View>
+            <Text style={{fontSize: 18}}>
+              Subtotal ({products.length} items):{' '}
+              <Text style={{color: '#e47911', fontWeight: 'bold'}}>
+                ${totalPrice.toFixed(2)}
+              </Text>
+            </Text>
+            <Button
+              text="Proceed to checkout"
+              onPress={onCheckout}
+              containerStyles={{
+                backgroundColor: '#f7e300',
+                borderColor: '#c7b702',
+              }}
+            />
+          </View>
+        )}
+      />
+        </View>
+    )
+}
+
+export default ShoppingCartScreen
+
+const styles = StyleSheet.create({
+    page: {
+        padding: 10,
+    },
+});
+
